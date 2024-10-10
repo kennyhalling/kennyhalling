bc584b5b5200525416948baade8f213eacb2170d
kennyhalling <122585704+kennyhalling@users.noreply.github.com>
2024-10-10 07:50:12 -0600

    Dice cleanup

--- coding-projects/dnd-app/dice.py
+++ coding-projects/dnd-app/dice.py
@@ -2,5 +2,22 @@
 import random
 
 class Dice:
-    def roll(sides):
-        print(random.randrange(1, sides))
+    # initialize number of sides on a dice
+    def __init__(self, sides):
+        if sides <= 0:
+            raise ValueError("The number of sides must be greater than 0")
+        self._sides = sides
+    
+    # roll the dice
+    def roll(self):
+        return random.randrange(1, self._sides + 1)
+    
+    #get number of sides
+    def get_sides(self):
+        return self._sides
+    
+    #set number of sides to a different number
+    def set_sides(self, sides):
+        if sides <= 0:
+            raise ValueError("The number of sides must be greater than 0")
+        self._sides = sides
--- coding-projects/dnd-app/tabletop.py
+++ coding-projects/dnd-app/tabletop.py
@@ -1,13 +1,6 @@
 # The virtual tabletop (until I figure out how to make a GUI)
-import os
-from dice import Dice
+# import os
+# from dice import Dice
 
-
-os.system('cls')
-Dice.roll(4)
-Dice.roll(6)
-Dice.roll(8)
-Dice.roll(10)
-Dice.roll(12)
-Dice.roll(20)
-Dice.roll(100)
+# class Tabletop:
+#     def __init__(self):
