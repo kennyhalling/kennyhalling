5ee88503ff2e0863c1592ac4d0c97ed4dc8ec9d4
kennyhalling <122585704+kennyhalling@users.noreply.github.com>
2024-10-12 00:06:39 -0600

    Started tkinter

--- coding-projects/dnd-app/tabletop.py
+++ coding-projects/dnd-app/tabletop.py
@@ -1,6 +1,13 @@
 # The virtual tabletop (until I figure out how to make a GUI)
 # import os
-# from dice import Dice
 
 # class Tabletop:
 #     def __init__(self):
+import tkinter as tk
+from dice import Dice
+
+root = tk.Tk()
+root.title("Technical Adventuring Companion (T.A.C.)")
+root.geometry("500x500")
+
+root.mainloop()
